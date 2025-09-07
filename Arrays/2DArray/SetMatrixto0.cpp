96ea310bd2cb6ff7e221640659cbf6b428cb6193
piyushxwork <piyush.coder21@gmail.com>
2025-09-07 22:45:50 +0530

    Initial commit

--- Arrays/2DArray/SetMatrixto0.cpp
+++ Arrays/2DArray/SetMatrixto0.cpp
@@ -1,41 +1,71 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
-void setRowAsMinusOne(int arr[4][4], int n, int m, int i){
-    for(int j=0; j<m; j++){
-        if(arr[i][j] != 0 && arr[i][j] != -1){
-            arr[i][j] = -1;
-        }
-    }
-}
+// void setRowAsMinusOne(int arr[4][4], int n, int m, int i){
+//     for(int j=0; j<m; j++){
+//         if(arr[i][j] != 0 && arr[i][j] != -1){
+//             arr[i][j] = -1;
+//         }
+//     }
+// }
 
-void setColAsMinusOne(int arr[4][4], int n, int m, int j){
-    for(int i=0; i<n; i++){
-        if(arr[i][j] != 0 && arr[i][j] != -1){
-            arr[i][j] = -1;
-        }
-    }
-}
+// void setColAsMinusOne(int arr[4][4], int n, int m, int j){
+//     for(int i=0; i<n; i++){
+//         if(arr[i][j] != 0 && arr[i][j] != -1){
+//             arr[i][j] = -1;
+//         }
+//     }
+// }
 
-void SetMatrix(int arr[4][4], int n, int m){
+// void SetMatrix(int arr[4][4], int n, int m){
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<m; j++){
+//             if(arr[i][j] == 0){
+//                 setRowAsMinusOne(arr,n,m,i);
+//                 setColAsMinusOne(arr,n,m,j);
+//             }
+//         }
+//     }
+
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<m; j++){
+//             if(arr[i][j] == -1){
+//                 arr[i][j] = 0;
+//             }
+//         }
+//     }
+// }
+
+
+void Setmatrix(int arr[4][4], int n, int m){
+    vector<int> row(n, 0);
+    vector<int> col(m,0);
+
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(arr[i][j] == 0){
-                setRowAsMinusOne(arr,n,m,i);
-                setColAsMinusOne(arr,n,m,j);
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
 
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(arr[i][j] == -1){
+            if(row[i] || col[j]){
                 arr[i][j] = 0;
             }
         }
     }
 }
+
+
+
+
+
+
 int main(){
     int n = 4, m = 4;
     int arr[4][4] = { {1,1,1,1},
@@ -43,7 +73,7 @@ int main(){
                       {1,1,0,1},
                       {1,1,1,1} };
 
-    SetMatrix(arr, n, m);
+    Setmatrix(arr, n, m);
 
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
